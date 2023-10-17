782cbdbacf9c5794c3190184583eb5b0b7ba95cc
Zehua Chen <zehua-chen@outlook.com>
2023-10-16 20:11:11 -0700

    [feat] Add score counter

--- godot/Player.gd
+++ godot/Player.gd
@@ -2,16 +2,16 @@ class_name Player
 extends RigidBody3D
 
 @export var speed = 3.0
-var score = 0
+var score = 0:
+	get:
+		return score
+	set(value):
+		$Label.text = "Score: %s" % value
+		score = value
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	score = 0
 	
 func _input(event):
 	if event.is_action("player_move_forward"):
